0b4081240bf034f3f34e79068c785bef9c29a58f
adarshTS <adarsh.ts8@gmail.com>
2023-09-11 18:03:40 +0530

    percy/sdk-utils

--- background/messages/finalize-build.ts
+++ background/messages/finalize-build.ts
@@ -1,19 +1,49 @@
+import { isPercyEnabled, postSnapshot } from "@percy/sdk-utils"
+
 import type { PlasmoMessaging } from "@plasmohq/messaging"
-import { PercyBuild } from "~schemas/build"
+
+import type { PercyBuild } from "~schemas/build"
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-    if (req.name !== 'finalize-build') return;
+  if (req.name !== "finalize-build") return
 
-    // Check if Percy Local Server is running
-    // If percy is not running
-    /**
+  // Check if Percy Local Server is running
+  // If percy is not running
+  /**
      * chrome.tabs.create({
                 url: "./tabs/start-percy.html"
             })
      */
-    const Build = await chrome.storage.local.get('percyBuild').then((res)=>res.percyBuild as PercyBuild);
 
-    // Loop through snaphshots and upload them.
+  const Build = await chrome.storage.local
+    .get("percyBuild")
+    .then((res) => res.percyBuild as PercyBuild)
+
+  const percyEnabled = await isPercyEnabled()
+  console.log("percyEnabled" + percyEnabled)
+
+  if (Build && percyEnabled) {
+    const snapshots = Build.snapshots
+    for (const snapshot of snapshots) {
+      const percySnapshot = {
+        // required
+        name: snapshot.options.name,
+        url: "",
+        domSnapshot: snapshot.dom,
+        // optional
+        environmentInfo: [],
+        clientInfo: "",
+        widths: snapshot.options.widths,
+        minHeight: snapshot.options["min-height"],
+        enableJavaScript: snapshot.options["enable-javascript"],
+        requestHeaders: {}
+      }
+      //   await postSnapshot(percySnapshot)
+      console.log("Snapshot post complete", percySnapshot)
+    }
+  } else {
+    console.log("Percy not enabled")
+  }
 }
 
 export default handler
--- views/capture.view.tsx
+++ views/capture.view.tsx
@@ -1,61 +1,105 @@
-import { sendToContentScript } from "@plasmohq/messaging";
-import { Button, Divider, Form, Input, message, Modal, Space } from "antd";
-import React, { useState } from "react";
-import SnapshotForm from "~components/snapshot.form";
-import { useAutoCapture } from "~hooks/use-autocapture";
-import { DeleteBuild } from "~utils/build";
+import { Button, Divider, Form, Input, message, Modal, Space } from "antd"
+import React, { useState } from "react"
+import { action } from "webdriverio/build/commands/browser"
+
+import { sendToBackground, sendToContentScript } from "@plasmohq/messaging"
+
+import SnapshotForm from "~components/snapshot.form"
+import { useAutoCapture } from "~hooks/use-autocapture"
+import { DeleteBuild } from "~utils/build"
 
 export function CaptureView() {
-    const [form] = Form.useForm()
-    const { autoCapture, ToggleAutoCapture } = useAutoCapture()
-    const [capturing,SetCapturing] = useState(false)
-    const actions = {
-        capture: () => {
-            SetCapturing(true)
-            form.validateFields().then(async (options)=>{
-                console.debug(`Snapshot Options: ${JSON.stringify(options, undefined, 2)}`)
-                await sendToContentScript({
-                    name: 'take_snapshot',
-                    body: options
-                }).then(() => {
-                    form.resetFields(['name']);
-                    message.success("Snapshot Captured!")
-                }).catch((err)=>{
-                    console.error(err)
-                    message.error(err)
-                })
-            }).catch(console.error).finally(()=>{
-                SetCapturing(false)
+  const [form] = Form.useForm()
+  const { autoCapture, ToggleAutoCapture } = useAutoCapture()
+  const [capturing, SetCapturing] = useState(false)
+  const actions = {
+    capture: () => {
+      SetCapturing(true)
+      form
+        .validateFields()
+        .then(async (options) => {
+          console.debug(
+            `Snapshot Options: ${JSON.stringify(options, undefined, 2)}`
+          )
+          await sendToContentScript({
+            name: "take_snapshot",
+            body: options
+          })
+            .then(() => {
+              form.resetFields(["name"])
+              message.success("Snapshot Captured!")
             })
-        },
-        cancelBuild: () => {
-            DeleteBuild()
-        },
-        viewSnapshots: () => {
-            chrome.tabs.create({
-                url: "./tabs/snapshots.html"
+            .catch((err) => {
+              console.error(err)
+              message.error(err)
             })
-        },
-        toggleCapture: () => {
-            ToggleAutoCapture()
-        }
+        })
+        .catch(console.error)
+        .finally(() => {
+          SetCapturing(false)
+        })
+    },
+    cancelBuild: () => {
+      DeleteBuild()
+    },
+    finalise: () => {
+      sendToBackground({
+        name: "finalize-build"
+      })
+    },
+    viewSnapshots: () => {
+      chrome.tabs.create({
+        url: "./tabs/snapshots.html"
+      })
+    },
+    toggleCapture: () => {
+      ToggleAutoCapture()
     }
+  }
 
-    return (
-        <React.Fragment>
-            <Form form={form} layout="vertical">
-                <SnapshotForm />
-            </Form>
-            <Space className="w-100" direction="vertical">
-                <Button loading={capturing} onClick={actions.capture} block size="large" >Capture Snapshot</Button>
-                <Button danger={autoCapture} onClick={actions.toggleCapture} block size="large" type="primary" >{autoCapture ? "Stop Auto Capture" : "Start Auto Capture"}</Button>
-            </Space>
-            <Divider />
-            <Space className="w-100" direction="vertical" >
-                <Button onClick={actions.viewSnapshots} block size="large" type="dashed" >View Snapshots</Button>
-                <Button block size="large" type="primary" >Finalize</Button>
-                <Button onClick={actions.cancelBuild} block size="large" danger type="text" >Cancel Build</Button>
-            </Space>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <Form form={form} layout="vertical">
+        <SnapshotForm />
+      </Form>
+      <Space className="w-100" direction="vertical">
+        <Button
+          loading={capturing}
+          onClick={actions.capture}
+          block
+          size="large">
+          Capture Snapshot
+        </Button>
+        <Button
+          danger={autoCapture}
+          onClick={actions.toggleCapture}
+          block
+          size="large"
+          type="primary">
+          {autoCapture ? "Stop Auto Capture" : "Start Auto Capture"}
+        </Button>
+      </Space>
+      <Divider />
+      <Space className="w-100" direction="vertical">
+        <Button
+          onClick={actions.viewSnapshots}
+          block
+          size="large"
+          type="dashed">
+          View Snapshots
+        </Button>
+        <Button onClick={actions.finalise} block size="large" type="primary">
+          Finalize
+        </Button>
+        <Button
+          onClick={actions.cancelBuild}
+          block
+          size="large"
+          danger
+          type="text">
+          Cancel Build
+        </Button>
+      </Space>
+    </React.Fragment>
+  )
 }
